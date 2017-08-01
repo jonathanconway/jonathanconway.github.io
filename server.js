0cff7bcb91d026149c7cafe86c328dbea39016da
Jonathan Conway <jonathan.conway@gmail.com>
2017-08-01 17:18:25 +1000

    feat/redirect prototypecontrols

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get('/westpacpr', redirect('http://www.zdnet.com/article/westpac-live-hits-2
 app.get('/tal', redirect('https://www.insuranceline.com.au/funeral-insurance'))
 app.get('/vmusic', redirect('https://web.archive.org/web/20120415120320/http://www.vmusic.com.au/home'))
 app.get('/healthdirect', redirect('http://www.zdnet.com/article/healthdirect-australia-looks-through-the-software-lens/'))
+app.get('/prototypecontrols', redirect('https://www.slideshare.net/jonathanaconway/working-with-researchers'))
 
 // Tech
 app.get('/how-to-net-development-on-mac', redirect('http://conwy.tech/net-development-on-macos-8tlwaj9g6'))
